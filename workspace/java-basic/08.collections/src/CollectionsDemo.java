0fa77bd2e091298f483ef14bc9919c11a4d6f77a
C <C@ORC-PC>
2020-02-08 13:51:35 +0900

    20200208 collections, exception

---
+++ workspace/java-basic/08.collections/src/CollectionsDemo.java
@@ -0,0 +1,185 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Properties;
+import java.util.Set;
+import java.util.Vector;
+
+public class CollectionsDemo {
+
+	public static void main(String[] args) throws Exception {
+
+
+//		//1. ArrayList
+//		//ArrayList al = new ArrayList();//Non Generic 방식
+//		ArrayList<String> al = new ArrayList<>();//Generic 방식
+//		al.add("첫 번째 데이터.");//목록의 마지막에 추가
+//		al.add("두 번째 데이터.");
+//		al.add("세 번째 데이터.");
+//		al.add("네 번째 데이터.");
+//		al.add("네 번째 데이터.");
+//		al.add(1, "삽입된 데이터.");//1번째 위치에 삽입		
+//		for (int i = 0; i < al.size(); i++) {//컬렉션.size() : 요소의 갯수
+//			System.out.println(al.get(i));//List컬렉션.get(위치) : 지정된 위치의 데이터 반환
+//		}		
+//		al.remove(2);//위치가 2번째 데이터 삭제
+//		String strToRemove = (String)al.get(0);
+//		al.remove(strToRemove);//strToRemove와 같은 객체를 제거 (여기서는 0번째 데이터)		
+//		System.out.println();
+//		for (int i = 0; i < al.size(); i++) {//컬렉션.size() : 요소의 갯수
+//			System.out.println(al.get(i));//List컬렉션.get(위치) : 지정된 위치의 데이터 반환
+//		}
+		
+		/////////////////////////////////////////////////////////////////////////////
+		
+		//2. Vector
+//		Vector<String> al = new Vector<>();//
+//		al.add("첫 번째 데이터.");//목록의 마지막에 추가
+//		al.add("두 번째 데이터.");
+//		al.add("세 번째 데이터.");
+//		al.add("네 번째 데이터.");
+//		al.add(1, "삽입된 데이터.");//1번째 위치에 삽입
+//		
+//		for (int i = 0; i < al.size(); i++) {//컬렉션.size() : 요소의 갯수
+//			System.out.println(al.get(i));//List컬렉션.get(위치) : 지정된 위치의 데이터 반환
+//		}
+//		
+//		al.remove(2);//위치가 2번째 데이터 삭제
+//		String strToRemove = al.get(0);
+//		al.remove(strToRemove);//strToRemove와 같은 객체를 제거 (여기서는 0번째 데이터)
+//		
+//		System.out.println();
+//		for (int i = 0; i < al.size(); i++) {//컬렉션.size() : 요소의 갯수
+//			System.out.println(al.get(i));//List컬렉션.get(위치) : 지정된 위치의 데이터 반환
+//		}
+		
+		/////////////////////////////////////////////////////////
+		
+		//3. LinkedList
+//		LinkedList<String> al = new LinkedList<>();//
+//		al.add("첫 번째 데이터.");//목록의 마지막에 추가
+//		al.add("두 번째 데이터.");
+//		al.add("세 번째 데이터.");
+//		al.add("네 번째 데이터.");
+//		al.add(1, "삽입된 데이터.");//1번째 위치에 삽입
+//		
+//		for (int i = 0; i < al.size(); i++) {//컬렉션.size() : 요소의 갯수
+//			System.out.println(al.get(i));//List컬렉션.get(위치) : 지정된 위치의 데이터 반환
+//		}
+//		
+//		al.remove(2);//위치가 2번째 데이터 삭제
+//		String strToRemove = al.get(0);
+//		al.remove(strToRemove);//strToRemove와 같은 객체를 제거 (여기서는 0번째 데이터)
+//		
+//		System.out.println();
+//		for (int i = 0; i < al.size(); i++) {//컬렉션.size() : 요소의 갯수
+//			System.out.println(al.get(i));//List컬렉션.get(위치) : 지정된 위치의 데이터 반환
+//		}
+		
+		/////////////////////////////////////////////////////////////////
+		
+		//4. HashSet
+//		HashSet<String> al = new HashSet<>();//
+//		al.add("첫 번째 데이터.");
+//		al.add("두 번째 데이터.");
+//		al.add("세 번째 데이터.");
+//		al.add("네 번째 데이터.");
+//		al.add("네 번째 데이터.");//추가 실패 (중복데이터)
+//		
+//		//for (int i = 0; i < al.size(); i++) {
+//		//	al.get(i);//Set 계열은 순서에 기반한 위치 정보 사용 X
+//		// 아래 : 인핸스드 for뮨
+//		for (String str : al) {//목록에서 다음 데이터가 있으면 데이터를 읽어서 str 저장
+//			System.out.println(str);
+//		}
+//		
+//		System.out.println();
+//		
+//		//al.remove(2);//오류 : 위치 번호 사용 불가능
+//		String strToRemove = "세 번째 데이터.";
+//		al.remove(strToRemove);//strToRemove와 같은 객체를 제거
+//		
+//		for (String str : al) {//목록에서 다음 데이터가 있으면 데이터를 읽어서 str 저장
+//			System.out.println(str);
+//		}
+//		
+//		System.out.println();
+		
+		////////////////////////////////////////////////////////
+		
+		//5. HashMap (Hashtable)
+//		//HashMap<String, String> al = new HashMap<>();// Thread Unsafe함 /*ArrayList 같은*/
+//		Hashtable<String, String> al = new Hashtable<>();// Thread Safe 함 /* Vector 같은 */ 
+//		al.put("1", "첫 번째 데이터.");
+//		al.put("2", "두 번째 데이터.");
+//		al.put("3", "세 번째 데이터.");
+//		al.put("4", "네 번째 데이터.");
+//		al.put("5", "네 번째 데이터.");//삽입 성공
+//		al.put("5", "다섯 번째 데이터.");//삽입 실패 (overwrite)
+//		
+//		System.out.println( al.get("4") );
+//		
+//		Set<String> keys = al.keySet();//키 목록 반환
+//		for (String key : keys) {//목록에서 다음 데이터가 있으면 데이터를 읽어서 str 저장
+//			String value = al.get(key);//목록에서 key로 검색된 데이터 반환
+//			System.out.println( value );
+//		}				
+//		String keyToRemove = "3";
+//		al.remove(keyToRemove);		
+//		System.out.println();		
+//		for (String key : al.keySet()) {
+//			System.out.println(al.get(key));
+//		}
+		
+		//////////////////////////////////////////////
+		
+		 //6. Properties 
+//		 //   (키 : 문자열, 값 : 문자열인 Map / 파일에 저장하고 읽는 기능 제공 / 주로 설정정보 관리용으로 사용)
+//		 Properties al = new Properties();//
+//		 al.put("1", "첫 번째 데이터.");
+//		 al.put("3", "세 번째 데이터.");
+//		 al.put("4", "네 번째 데이터.");
+//		 al.put("2", "두 번째 데이터.");
+//		 al.put("5", "네 번째 데이터.");//삽입 성공
+//		 al.put("5", "다섯 번째 데이터.");//삽입 실패
+//		
+//		 //파일에 데이터를 저장하는 명령
+//		 al.store(new FileOutputStream("test.properties"), "test properties file");
+//		
+//		 Properties props = new Properties();
+//		 props.load(new FileInputStream("test.properties"));//파일에서 데이터 읽는 명령
+//		
+//		 Set keys = props.keySet();//키 목록 반환
+//		 for (Object key : keys) {//목록에서 다음 데이터가 있으면 데이터를 읽어서 str 저장
+//		 	String value = (String)props.get(key);//목록에서 key로 검색된 데이터 반환
+//		 	System.out.println( value );
+//		}
+		
+		//7. Stack (후입선출 - Last In First Out)
+//		Stack<String> stack = new Stack<>();
+//		stack.push("a;dklja;sldkjfa;sdfjl");
+//		stack.push("13243654321321");
+//		stack.push("ㅁ;ㄴ아ㅓㄻ;니ㅏ얼");
+//		
+//		while (stack.size() > 0) {
+//			System.out.println(stack.pop());	//읽고 목록에서 제거
+//			//System.out.println(stack.peek());	//읽고 목록에 유지
+//		}	
+		
+		//8. Queue (선입선출 - First In First Out)
+//		Queue<String> queue = new LinkedList<>();
+//		queue.offer("a;dklja;sldkjfa;sdfjl");
+//		queue.offer("13243654321321");
+//		queue.offer("ㅁ;ㄴ아ㅓㄻ;니ㅏ얼");
+//		
+//		while (queue.size() > 0) {
+//			System.out.println(queue.poll());	//읽고 목록에서 제거
+//			//System.out.println(queue.peek());	//읽고 목록에 유지
+//		}
+
+	}
+
+}
